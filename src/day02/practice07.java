1eaa2c467d560b4dcc2c00417aafcb9f5fd2fc17
test123 <aa@aaa.com>
2024-02-06 17:19:00 +0900

    day02

--- src/day02/practice07.java
+++ src/day02/practice07.java
@@ -6,32 +6,32 @@ public class practice07 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		String name = null;
-		System.out.print("학생 이름 : " + name);
+		String name;
+		System.out.print("학생 이름 : ");
 		name = input.next();
 		
-		int kor = 0;
-		System.out.print("국어 점수 : " + kor);
+		int kor;
+		System.out.print("국어 점수 : ");
 		kor = input.nextInt();
 		
-		int eng = 0;
-		System.out.print("영어 점수 : " + eng);
+		int eng;
+		System.out.print("영어 점수 : ");
 		eng = input.nextInt();
 		
-		int math = 0;
-		System.out.print("수학 점수 : " + math);
+		int math;
+		System.out.print("수학 점수 : ");
 		math = input.nextInt();
 		
-		int sum = 0;
+		int sum;
 		sum = kor + eng + math;
 		
-		int everage = 0;
+		int everage;
 		everage = sum/3;
 		
 		System.out.println("===================학생 정보===================");
 		System.out.println("이름\t국어\t영어\t수학\t합계\t평균");
 		System.out.println("---------------------------------------------");
-		System.out.println(name + kor + eng + math + sum + everage );
+		System.out.println(name+ "\t" + kor+ "\t" + eng+ "\t" + math + "\t" + sum  + "\t" + everage );
 		
 	}
 }
